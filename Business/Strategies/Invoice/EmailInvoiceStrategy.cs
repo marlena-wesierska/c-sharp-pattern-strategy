fbdaf4515a433efe33f0d57fbf742da863ffd199
MW <marlena.wesierska@gmail.com>
2021-10-02 16:35:07 +0200

    first commit

---
+++ Business/Strategies/Invoice/EmailInvoiceStrategy.cs
@@ -0,0 +1,28 @@
+﻿using Strategy_Pattern_First_Look.Business.Models;
+using System.Net;
+using System.Net.Mail;
+
+namespace Strategy_Pattern_First_Look.Business.Strategies.Invoice
+{
+    public class EmailInvoiceStrategy : InvoiceStrategy
+    {
+        public override void Generate(Order order)
+        {
+            var body = GenerateTextInvoice(order);
+
+            using (SmtpClient client = new SmtpClient("smtp.sendgrid.net", 587))
+            {
+                NetworkCredential credentials = new NetworkCredential("USERNAME", "PASSWORD");
+                client.Credentials = credentials;
+
+                MailMessage mail = new MailMessage("YOUR EMAIL", "YOUR EMAIL")
+                {
+                    Subject = "We've created an invoice for your order",
+                    Body = body
+                };
+
+                client.Send(mail);
+            }
+        }
+    }
+}
---
+++ Business/Strategies/Invoice/PrintOnDemandInvoiceStrategy.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using Strategy_Pattern_First_Look.Business.Models;
+using System;
+using System.Net.Http;
+
+namespace Strategy_Pattern_First_Look.Business.Strategies.Invoice
+{
+    public class PrintOnDemandInvoiceStrategy : IInvoiceStrategy
+    {
+        public void Generate(Order order)
+        {
+            using (var client = new HttpClient())
+            {
+                var content = JsonConvert.SerializeObject(order);
+
+                client.BaseAddress = new Uri("https://pluralsight.com");
+
+                client.PostAsync("/print-on-demand", new StringContent(content));
+            }
+        }       
+    }
+}
